c92aed2a017781618963864b3dee6feca13aed60
songhee24 <akaratel61@gmail.com>
2022-08-24 16:20:26 +0600

    react router 6 new basics created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import Home from "./pages/home/Home";
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import New from './pages/new/New'
+import List from './pages/list/List'
+import Single from './pages/single/Single'
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route path='/'>
+                    <Route index element={<Home/>} />
+                    <Route path='login' element={<Login/>} />
+                    <Route path='users'>
+                        <Route index element={<List/>} />
+                        <Route path='new' element={<New/>} />
+                        <Route path=':userId' element={<Single />}  />
+                    </Route>
+                    <Route path='products'>
+                        <Route index element={<List/>} />
+                        <Route path='new' element={<New/>} />
+                        <Route path=':productId' element={<Single />}  />
+                    </Route>
+                </Route>
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
