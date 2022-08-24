035224ac5221575f9f93275215c22b8a8e29de14
songhee24 <akaratel61@gmail.com>
2022-08-24 17:24:50 +0600

    simple base sidebar layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,30 @@ import React from 'react';
 import './sidebar.scss';
 
 const Sidebar = () => {
-    return <div>sidebar</div>;
+    return (
+        <div className="sidebar">
+            <div className="top">
+                <div className="logo">Songhee24 Admin</div>
+            </div>
+            <div className="center">
+                <ul>
+                    <li>
+                        <span>Dashboard </span>
+                    </li>
+                    <li>
+                        <span>Dashboard </span>
+                    </li>
+                    <li>
+                        <span>Dashboard </span>
+                    </li>
+                    <li>
+                        <span>Dashboard </span>
+                    </li>
+                </ul>
+            </div>
+            <div className="bottom">color options</div>
+        </div>
+    );
 };
 
 export default Sidebar;
