11670479332f225454e7a7b584243db7bdf530b5
songhee24 <akaratel61@gmail.com>
2022-08-25 20:50:34 +0600

    simple base circle progress bar

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 import './featured.scss';
 
@@ -11,7 +13,20 @@ const Featured = () => {
                 <MoreVertOutlinedIcon fontSize="small" />
             </div>
             <div className="bottom">
-                <div className="featuredChart"></div>
+                <div className="featuredChart">
+                    <CircularProgressbar
+                        minValue={0.1}
+                        value={70}
+                        text={'70%'}
+                        strokeWidth={4}
+                    />
+                </div>
+                <p className="title">Total sales made today</p>
+                <p className="amount">420 $</p>
+                <p className="desc">
+                    Previous transactions processing, Last payments may not be
+                    included
+                </p>
             </div>
         </div>
     );
